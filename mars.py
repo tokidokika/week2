3e77c02f2e67d39035505bfb393c3f07ae50bcb3
Kate <yukaicat@gmail.com>
2020-02-22 18:23:05 +0300

    saturday changes

--- mars.py
+++ mars.py
@@ -1,6 +1,27 @@
 import ephem
 
-mars = ephem.Mars('2000/01/01')
-const = ephem.constellation(mars)
+# mars = ephem.Mars('2000/01/01')
+# const = ephem.constellation(mars)
 
-print(const)
+# print(const)
+
+'''Написать функцию, которая при вводе планеты пользователем выдаёт ее положение в созвездии на today'''
+
+def planets(planet):
+    date = '2000/01/01'
+    user_say = planet
+    dct = {
+        'Pluto': ephem.Pluto
+    }
+    planet_func = dct[user_say]
+    planet_obj = planet_func(date)
+    planet_cons = ephem.constellation(planet_obj)
+    print(planet_cons[1])
+    # if user_say == 'Pluto':
+    #     plt = ephem.Pluto(date)
+    #     print(user_say, date, ephem.constellation(plt))
+    # elif user_say == 'Mercury':
+    #     mrc = ephem.Merc(date)
+    #     # print(user_say 'на дату' date, ephem.constellation(plt)
+
+planets('Pluto')
